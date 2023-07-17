96a7e7af5ae2d9f3445ef71f0ef055662d9ffbbe
GracePearcey <pearcey.grace@gmail.com>
2023-07-17 16:43:20 -0230

    removed add and remove delay functions

--- src/native_functions.rs
+++ src/native_functions.rs
@@ -11,12 +11,6 @@ extern "C" {
     #[link_name = "PrintInt32"]
     pub fn PrintInt32(input: i32, hex: i32);
 
-    #[link_name = "AddAppDelay"]
-    pub fn AddAppDelay();
-
-    #[link_name = "RemoveAppDelay"]
-    pub fn RemoveAppDelay();
-    
     /// Sends a NMEA message and specifies the controller queue it should be sent to  
     #[link_name = "SendMsg"]
     pub fn SendMsg(controller_number: i32, priority: i32, PGN: i32, source: i32, data: *const u8, data_length_bytes: i32 ) -> i32;
